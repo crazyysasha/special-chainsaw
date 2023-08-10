f4f628fd92ecc09753f147fb9f920e74926f3f6d
Odilbek14 <lol_1717@inbox.ru>
2023-08-10 21:59:29 +0500

    Ozodbek/11-6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html",
-  "./src/**/*.{vue,js}"],
+  content: [  "./index.html",
+  "./src/**/*.{vue,js}",
+],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
