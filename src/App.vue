82b2aeb12c7efac7eb5526f1554feb676ef0f21a
Odilbek14 <lol_1717@inbox.ru>
2023-08-14 15:35:14 +0500

    Ozodbek/13-classwork

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
 </script>
 <template>
-  <div class="text-red-500">
-    Hello world!
+  <div class="">
+    <input type="text" placeholder="Name">
+    <input type="text" placeholder="Surname">
+    <input type="text" placeholder="Email">
+    <input type="password" placeholder="password">
+    <button v-bind="class">Send</button>
+    {{ massage }}
   </div>
 </template>
