1be10715fd281d0e917466e955d940fba32baac6
ozolox123 <lol_1717@inbox.ru>
2023-08-19 13:03:03 +0300

    fv

---
+++ App.vue
@@ -0,0 +1,37 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+@font-face {
+  font-family: "Poppins-Bold";
+  src: local("Poppins-Bold"), url("./assets/font/Poppins-Bold.ttf") format("opentype");
+}
+@font-face {
+  font-family: "Poppins-Black";
+  src: local("Poppins-Black"), url("./assets/font/Poppins-Black.ttf") format("opentype");
+}
+@font-face {
+  font-family: "Poppins-Light";
+  src: local("Poppins-Light"), url("./assets/font/Poppins-Light.ttf") format("opentype");
+}
+@font-face {
+  font-family: "Poppins-Medium";
+  src: local("Poppins-Medium"), url("./assets/font/Poppins-Medium.ttf") format("opentype");
+}
+@font-face {
+  font-family: "Poppins-Regular";
+  src: local("Poppins-Regular"), url("./assets/font/Poppins-Regular.ttf") format("opentype");
+}
+@font-face {
+  font-family: "Poppins-SemiBold";
+  src: local("Poppins-Semi-Bold"), url("./assets/font/Poppins-SemiBold.ttf") format("opentype");
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  -webkit-font-smoothing : antialiased;
+}
+</style>
